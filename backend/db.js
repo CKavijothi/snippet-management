83a342e2c1dadd13cfcb7d50217783e232ed00e9
CKavijothi <ckavijothi@gmail.com>
2026-05-11 14:01:22 +0530

    Update connection for Railway

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", 
-  database: "snippetdb",
-  port: 3307 
+  // If online, use Railway's address. If at home, use localhost.
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "",
+  database: process.env.MYSQLDATABASE || "snippetdb",
+  // Railway usually uses 3306; your local setup used 3307.
+  port: process.env.MYSQLPORT || 3307
 });
 
 db.connect(err => {
